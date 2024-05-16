826761e965331545c30718aebaa365be9dc2eba7
Alexanderm2005 <146048237+Alexanderm2005@users.noreply.github.com>
2024-05-16 17:04:22 -0500

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CreaturesModule } from './creatures/creatures.module';
+import { OrmModule } from './orm/orm.module';
+
+@Module({
+  imports: [PrismaModule, CreaturesModule, OrmModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
